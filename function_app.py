d1c7af80c38c743ceeeefda5f6e84d59455f5807
BBB script repo <gpetrillo@bitxbit.com>
2025-07-16 16:39:07 -0400

    Anonymize function, protection is in azure network

--- function_app.py
+++ function_app.py
@@ -11,7 +11,7 @@ import azure.functions as func
 app = func.FunctionApp()
 
 # Define your HTTP trigger function using a decorator
-@app.route(route="GetBeyondTrustData", methods=["GET"])
+@app.route(route="GetBeyondTrustData", methods=["GET"], auth_level=func.AuthLevel.ANONYMOUS)
 def GetBeyondTrustData(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
